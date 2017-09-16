02deb4532a19ea9652c444c021ce7e49b1a9f46d
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2017-09-16 11:47:25 +0200

    Fix HTML-Structure

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<DOCTYPE! html>
-<html>
-
-<?php include "header.php"; ?>
-
-<body>
-  <?php  include "head.php"; ?>
-
-
-  <div id="articles">
-    <?php
-    include "article.php";
-
-    AllArticle();
-
-     ?>
-  </div>
-</body>
-
+<!DOCTYPE html>
+<html lang="YOURLANG">
+  <head>
+    <meta charset="utf-8"/>
+    <!--Missing Viewport, Favicon and so far...-->
+    <title>Les création du Poney</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <img id="banner" src="http://blanc.leponeyblanc.fr/wp-content/uploads/2014/01/pinkie_pie_by_pony_spiz-d477lwt.png" alt="banner">
+    <?php include "menue.php" ?>
+    <div id="articles">
+      <?php
+      include "article.php";
+      AllArticle();
+       ?>
+    </div>
+  </body>
 </html>
